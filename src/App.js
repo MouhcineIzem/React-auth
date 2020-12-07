355c02a9b44283bf5e18be24f4c663106ed8a0e7
fadibannani93 <fadibannani93@gmail.com>
2020-12-07 17:22:20 +0100

    finnishing the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import {BrowserRouter as Router , Route, Switch, Link} from 'react-router-dom';
+import React, {useState} from 'react';
+import Dashboard from "./components/Dashboard/Dashboard";
+import Preferences from "./components/Preferences/Preferences";
+import Login from "./components/Login/Login";
 import './App.css';
 
 function App() {
+    const [token, setToken] = useState();
+    if(!token) {
+        return <Login setToken={setToken} />
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Application</h1>
+        <Router>
+          <Switch>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+            <Route path="/preferences">
+              <Preferences />
+            </Route>
+          </Switch>
+        </Router>
     </div>
   );
 }
